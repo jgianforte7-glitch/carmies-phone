ea3305c81f686c2513b45bb06cbfa9ef543605bc
jgianforte7-glitch <jgianforte7@gmail.com>
2026-07-31 13:04:22 -0500

    Update getPhotos.js

--- netlify/functions/getPhotos.js
+++ netlify/functions/getPhotos.js
@@ -1,19 +1,11 @@
+const manifest = require('./photos-manifest.json');
+
 exports.handler = async (event, context) => {
   try {
-    // On Netlify, deployed files are at the root
-    // We'll try to fetch the photos by checking their URLs
-    // Since we can't read server files directly, list them manually or use a workaround
-    
-    // For now, return empty - we'll use a different approach
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify([
-        { file: '126.jpg', type: 'image', name: '126' },
-        { file: '176.jpg', type: 'image', name: '176' },
-        { file: 'Profile Pic.jpg', type: 'image', name: 'Profile Pic' },
-        { file: '60382498036_0E58985F-C698-4B17-A3D9-75A7FCF35381.JPG', type: 'image', name: 'Photo' }
-      ])
+      body: JSON.stringify(manifest)
     };
   } catch (error) {
     return {
